da8a1527692160cac197c907b61580de005e3b3f
lucasreynes <93935044+lucasreynes@users.noreply.github.com>
2021-11-22 17:22:15 +0100

    Update House.py

--- src/House.py
+++ src/House.py
@@ -1,45 +1,50 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Thu Nov 16 19:47:50 2017
-
 @author: lfoul
 """
 import OpenGL.GL as gl
 
 class House:
-    # Constructor
-    def __init__(self, parameters = {}) :  
-        # Parameters
-        # position: position of the house
-        # orientation: orientation of the house
+	# Constructor
+	def __init__(self, parameters = {}) :  
+		# Parameters
+		# position: position of the house
+		# orientation: orientation of the house
 
-        # Sets the parameters
-        self.parameters = parameters
+		# Sets the parameters
+		self.parameters = parameters
 
-        # Sets the default parameters 
-        if 'position' not in self.parameters:
-            self.parameters['position'] = [0, 0, 0]  
-        if 'orientation' not in self.parameters:
-            self.parameters['orientation'] = 0  
-                
-        # Objects list
-        self.objects = []
+		# Sets the default parameters 
+		if 'position' not in self.parameters:
+			self.parameters['position'] = [0, 0, 0]  
+		if 'orientation' not in self.parameters:
+			self.parameters['orientation'] = 0  
+				
+		# Objects list
+		self.objects = []
 
-    # Getter
-    def getParameter(self, parameterKey):
-        return self.parameters[parameterKey]
-    
-    # Setter
-    def setParameter(self, parameterKey, parameterValue):
-        self.parameters[parameterKey] = parameterValue
-        return self
-       
-    # Adds an object    
-    def add(self, x):
-        self.objects.append(x)
-        return self
-            
-    # Draws the house      
-    def draw(self):  
-        # A compléter en remplaçant pass par votre code
-        pass        
+	# Getter
+	def getParameter(self, parameterKey):
+		return self.parameters[parameterKey]
+	
+	# Setter
+	def setParameter(self, parameterKey, parameterValue):
+		self.parameters[parameterKey] = parameterValue
+		return self
+	   
+	# Adds an object    
+	def add(self, x):
+		self.objects.append(x)
+		return self
+			
+	# Draws the house      
+	def draw(self):  
+		# A compléter en remplaçant pass par votre code
+		gl.glMatrixMode(gl.GL_MODELVIEW)
+		gl.glPushMatrix()
+		gl.glTranslatef(self.parameters["position"][0], self.parameters["position"][1], self.parameters["position"][2] )
+		gl.glRotatef(self.parameters["orientation"], 0,0,1)
+		for obj3d in self.objects:
+			obj3d.draw() 
+		gl.glPopMatrix() 
