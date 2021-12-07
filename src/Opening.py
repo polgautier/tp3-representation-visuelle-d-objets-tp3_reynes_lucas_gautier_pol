3dd1ea13b6417fd1605eb8db180bcd52709e7014
polgautier <93935088+polgautier@users.noreply.github.com>
2021-12-07 16:58:10 +0100

    Update Opening.py

--- src/Opening.py
+++ src/Opening.py
@@ -30,95 +30,48 @@ class Opening:
 			raise Exception('Parameter "thickness" required.')    
 		if 'color' not in self.parameters:
 			raise Exception('Parameter "color" required.')  
+		if 'orientation' not in self.parameters:
+            		self.parameters['orientation'] = 0 
+
 			
 		# Generates the opening from parameters
 		self.generate()  
 
-	# Getter
-	def getParameter(self, parameterKey):
-		return self.parameters[parameterKey]
+		# Getter
+		def getParameter(self, parameterKey):
+			return self.parameters[parameterKey]
 	
-	# Setter
-	def setParameter(self, parameterKey, parameterValue):
+		# Setter
+		def setParameter(self, parameterKey, parameterValue):
 		self.parameters[parameterKey] = parameterValue
 		return self        
 
-	# Defines the vertices and faces        
-	def generate(self):
-		w = self.parameters['width'] ; h = self.parameters['height'] ; t = self.parameters['thickness']
-		
-		self.vertices = [ 
-			# devant pgauche
-			[0, 0, 0],
-			[0, 0, h], 
-			[0+t, 0, h],
-			[0+t, 0, 0],
-			# derriere pgauche
-			[0, t, 0], 
-			[0+t, t, 0], 
-			[0+t, t, h],
-			[0, t, h], 
-			# devant phaut
-			[0, 0, h-t],
-			[0, 0, h], 
-			[w, 0, h],
-			[w, 0, h-t],
-			# derriere phaut
-			[0, t, h-t], 
-			[w, t, h-t], 
-			[w, t, h],
-			[0, t, h], 	
-			# devant pdroite
-			[w, 0, 0],
-			[w, 0, h], 
-			[w-t, 0, h],
-			[w-t, 0, 0],
-			# derriere pdroite
-			[w, t, 0], 
-			[w-t, t, 0], 
-			[w-t, t, h],
-			[w, t, h],
-			# devant pbas
-			[0, 0, h-t],
-			[0, 0, h], 
-			[w, 0, h],
-			[w, 0, h-t],
-			# derriere pbas
-			[0, t, h-t], 
-			[w, t, h-t], 
-			[w, t, h],
-			[0, t, h]
-		]
-		self.faces = [ # sens anti-horaire
-			[0, 3, 2, 1], # devant   pgauche
-			[4, 7, 6, 5], # derriere pgauche
-			[0, 1, 7, 4], # gauche   pgauche
-			[2, 3, 5, 6], # droite   pgauche
-			[1, 2, 6, 7], # haut     pgauche
-			[3, 0, 4, 5],  # bas     pgauche
-			
-			[0, 3, 2, 1], # devant   pdoite
-			[4, 7, 6, 5], # derriere pdoite
-			[0, 1, 7, 4], # gauche   pdoite
-			[2, 3, 5, 6], # droite   pdoite
-			[1, 2, 6, 7], # haut     pdoite
-			[3, 0, 4, 5],  # bas     pdoite
-			
-			[0, 3, 2, 1], # devant   phaut
-			[4, 7, 6, 5], # derriere phaut
-			[0, 1, 7, 4], # gauche   phaut
-			[2, 3, 5, 6], # droite   phaut
-			[1, 2, 6, 7], # haut     phaut
-			[3, 0, 4, 5],  # bas     phaut
-			
-			[0, 3, 2, 1], # devant   pbas
-			[4, 7, 6, 5], # derriere pbas
-			[0, 1, 7, 4], # gauche   pbas
-			[2, 3, 5, 6], # droite   pbas
-			[1, 2, 6, 7], # haut     pbas
-			[3, 0, 4, 5],  # bas     pbas
-		]    
-		
+		# Defines the vertices and faces        
+		def generate(self):
+		x,y,z=self.parameters['position'][0],self.parameters['position'][1],self.parameters['position'][2]
+       		w=self.parameters['width']
+        	h=self.parameters['height']
+        	t=self.parameters['thickness']
+        	angle_deg=self.parameters['orientation']
+        	a=angle_deg*pi/180
+        
+        	self.vertices = [ 
+                	[x,y,z],
+                	[x,y,z+h],
+        	        [x+w*cos(a),y+w*sin(a),z+h],
+	                [x+w*cos(a),y+w*sin(a),z],
+                	[x-t*sin(a),y+t*cos(a),z],
+        	        [x-t*sin(a),y+t*cos(a),z+h],
+ 	                [x+w*cos(a)-t*sin(a),y+w*sin(a)+t*cos(a),z+h],
+                	[x+w*cos(a)-t*sin(a),y+w*sin(a)+t*cos(a),z]
+                ]
+        	self.faces = [
+                	[0,1,4,5],
+               		[2,3,6,7],
+                	[0,4,3,7],
+                	[1,5,2,6]
+                ]   
+
 	# Draws the faces                
 	def draw(self):        
         for elem in self.objects:
